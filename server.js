0b80631fdf311bd2bff2c916d0c605cd4ed218f3
jtyrmn <jaketyermanbusiness@gmail.com>
2022-03-05 22:35:26 -0800

    moved language processing logic onto server

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
-const path = require('path');
+const getResponse = require('./getResponse');
 const server = express();
 
-server.use('/', express.static(path.join('site')));
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+
+server.use('/', express.static('site'));
+
+server.post('/api/', (req, res) => {
+    const input = req.body.input;
+    console.log('input: ', input);
+    const output = getResponse(input);
+    console.log('output: ', output);
+    res.json({output: output});
+});
+
+server.get('/api/idea', (req, res) => {
+    const idea = getResponse.getIdea();
+    res.json({idea: idea});
+});
 
 server.listen(4000, () => {
     console.log('server running...');
